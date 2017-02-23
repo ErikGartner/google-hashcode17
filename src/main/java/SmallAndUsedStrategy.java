16d607c5f790c6aee851289b18c7edbe100804f1
Zkoogan <andreminemail@hotmail.com>
2017-02-23 19:55:44 +0100

    full shell smallandused

---
+++ src/main/java/SmallAndUsedStrategy.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by RTG on 2017-02-23.
+ */
+public class SmallAndUsedStrategy extends Strategy {
+
+    public void apply(Map<Integer, Cache> cacheMap, List<Request> requests, ArrayList<Video> videos, List<Endpoint> endpoints, int i) {
+
+        for(Cache c: cacheMap.values()){
+            int index = 0;
+            videos.sort();
+            int myAllocatedMemory = 0;
+            while(myAllocatedMemory > i){
+                c.videos.add(videos.get(index));
+                index++;
+            }
+        }
+    }
+}
