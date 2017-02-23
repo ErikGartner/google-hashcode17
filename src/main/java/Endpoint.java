e1ac478b011d780fe0fa0bca07794c4f341e3b35
johanahlqvist <johan_ahlkvist@hotmail.com>
2017-02-23 20:05:36 +0100

    endpoint feature

--- src/main/java/Endpoint.java
+++ src/main/java/Endpoint.java
@@ -1,6 +1,7 @@
 import java.util.List;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Created by erik on 2017-02-23.
@@ -9,15 +10,22 @@ public class Endpoint {
 
     public final int latency;
     public final Map<Cache, Integer> cacheLatency;
+    public Map<Cache, Integer> cacheGains = new HashMap<Cache, Integer>();
     public LinkedList<Request> requests;
 
     public Endpoint(int latency, Map<Cache, Integer> cacheLatency){
         this.cacheLatency = cacheLatency;
         this.latency = latency;
         requests = new LinkedList<Request>();
+        for(Cache c: cacheLatency.keySet()){
+            cacheGains.put(c, cacheLatency.get(c) - latency);
+        }
+
     }
 
     public void addRequest(Request r){
         requests.add(r);
     }
+
+
 }
