6fe15ee92281ee2bcbe31add1fa631a0d79c3961
Zkoogan <andreminemail@hotmail.com>
2017-02-23 20:19:51 +0100

    to pull

--- src/main/java/SmallAndUsedStrategy.java
+++ src/main/java/SmallAndUsedStrategy.java
@@ -1,23 +1,30 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by RTG on 2017-02-23.
  */
 public class SmallAndUsedStrategy extends Strategy {
 
-    public void apply(Map<Integer, Cache> cacheMap, List<Request> requests, ArrayList<Video> videos, List<Endpoint> endpoints, int i) {
+    int gainz = 0;
 
+    public void apply(Map<Integer, Cache> cacheMap, List<Request> requests, ArrayList<Video> videos, List<Endpoint> endpoints, int i) {
         for(Cache c: cacheMap.values()){
             int index = 0;
-           // videos.sort();
+            Collections.sort(videos);
             int myAllocatedMemory = 0;
-            while(myAllocatedMemory > i){
-                c.videos.add(videos.get(index));
+            while(myAllocatedMemory < i){
+                c.cacheVideo((videos.get(index)));
                 index++;
             }
+            for (Request r : requests){
+                if(c.videos.contains(r.video) && c.endpoints.contains(r.endpoint)){
+                    gainz += r.no*(r.endpoint.latency - r.endpoint.cacheLatency.get(c));
+                }
+            }
         }
+    }
+
+    public String toString(){
+        return "Latency saved =" + gainz;
     }
 }
